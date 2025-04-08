52087da0b74fdc464b2e81fb658d2c3f6e7517ac
Jaime Palomares <estebanpalomares23@gmail.com>
2025-04-08 11:54:20 -0500

    clean code

--- src/tests/api/autthTest.spec.js
+++ src/tests/api/autthTest.spec.js
@@ -1,5 +1,4 @@
 import {test, expect} from '@playwright/test';
-import { log } from 'console';
 
 test.describe('Authentication test', () => {
     test('Fail Bearer Authentication', async ({ request }) => {
